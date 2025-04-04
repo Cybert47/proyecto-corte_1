31a65b07d0cac2c14952bae7a98362a7a3bb8fcf
Esteban David Hernandez Parra <133552482+Cybert47@users.noreply.github.com>
2025-04-04 20:16:43 +0000

    menu

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 //  Configuraciones y constantes
 const CONFIG = {
   MENU_URL:
-    "https://script.google.com/macros/s/AKfycbxlUDs3QXpveCkhzvXDowUXFfiMPQef3U3MijRL-7hQp4OAB8aMHe-FgynFcqep0-D8/exec",
+    "https://script.google.com/macros/s/AKfycbzrFghQ8rX6Cx_KkOEER5QteZa5eg1TqCXvOK1S-suhsmxKoFXbVKYWbN9i-iM0jRu2/exec",
   ORDER_URL:
-    "https://script.google.com/macros/s/AKfycbxlUDs3QXpveCkhzvXDowUXFfiMPQef3U3MijRL-7hQp4OAB8aMHe-FgynFcqep0-D8/exec",
+    "https://script.google.com/macros/s/AKfycbzrFghQ8rX6Cx_KkOEER5QteZa5eg1TqCXvOK1S-suhsmxKoFXbVKYWbN9i-iM0jRu2/exec",
   CATEGORIES: ["starters", "main", "drinks", "desserts"],
   DEFAULT_CATEGORY: "desserts",
 }
